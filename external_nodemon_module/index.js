60fe953d2171e7e6f324786301ab47bc8a0d1aa4
mahabur  rahman <annur4395@gmail.com>
2022-01-04 23:29:43 +0600

    external module with creating a server

---
+++ external_nodemon_module/index.js
@@ -0,0 +1,5 @@
+// const randomFruitsName = require("random-fruits-name");
+// console.log(randomFruitsName());
+
+const randomMovieNames = require("random-movie-names");
+console.log(randomMovieNames());
