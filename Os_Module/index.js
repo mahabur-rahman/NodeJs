3273275623d8d3a41f4720d1931989ff131e6515
mahabur  rahman <annur4395@gmail.com>
2022-01-04 18:17:01 +0600

    os module

---
+++ Os_Module/index.js
@@ -0,0 +1,28 @@
+// const os = require("os");
+// console.log(os);
+
+// const { totalmem, freemem } = require("os");
+
+// console.log(os.userInfo());
+// const info = os.userInfo();
+// console.log(info.shell);
+// console.log(info.gid);
+// console.log(os.homedir());
+// console.log(os.hostname());
+
+// console.log(totalmem());
+// console.log(freemem());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// const path = require("path");
+// console.log(path);
+
+// const findFileExtension = path.extname("index.html");
+// console.log(findFileExtension);
+// const joinName = path.join(__dirname + "/test");
+// const joinName2 = path.join(__dirname + "/../views");
+// console.log(__dirname);
+// console.log(joinName);
+// console.log(joinName2);
