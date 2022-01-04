5e773c7599262f8bb75690a17d91c81fec520c12
mahabur  rahman <annur4395@gmail.com>
2022-01-04 10:53:14 +0600

    learning of node js

---
+++ Export_Import/index.js
@@ -0,0 +1,16 @@
+// const result = require("./student");
+// const s1 = require("./student");
+
+const { fullName } = require("./student");
+
+console.log(fullName());
+
+// --------------
+// console.log(s1.getName());
+// console.log(s1.getId());
+// console.log(s1.age());
+
+// ----------------
+// console.log(result.name());
+// console.log(result.id());
+// console.log(result.age());
---
+++ Export_Import/student.js
@@ -0,0 +1,25 @@
+const getName = () => {
+  return "mahabur rahman";
+};
+
+const getId = () => {
+  return "201531043008";
+};
+
+const age = () => {
+  return "25";
+};
+
+exports.fullName = () => {
+  return "Web developer with react js";
+};
+
+// exports.name = getName;
+// exports.id = getId;
+// exports.age = age;
+
+// module.exports = {
+//   getName,
+//   getId,
+//   age,
+// };
